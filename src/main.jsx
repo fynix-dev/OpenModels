9dbc7d740c46761b078f8e2d8328957ae8f162ea
Aadi Narayanan R <aadinarayanan330@gmail.com>
2025-11-11 14:06:52 +0530

    OpenModel v1 11-11-25 14:05

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CnnModel from './screens/CNNModel.jsx';
 import RnnModel from './screens/RnnModel.jsx';
 import NextWordPredictor from './screens/NextWordPredictor.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/cnn' element={<CnnModel />} />
-      <Route path='/rnn' element={<RnnModel />} />
+      <Route path='/' element={<RnnModel />} />
       <Route path='/next-word-predictor' element={<NextWordPredictor />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
