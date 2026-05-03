125ffc881433664142616aa68935d0c4fffbb38e
Aadi <aadinarayanan330@gmail.com>
2026-05-03 16:56:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CnnModel from "./screens/CnnModel.jsx";
 import NextWordPredictor from "./screens/NextWordPredictor";
 import RnnModel from "./screens/RnnModel";
+import ModelList from "./screens/ModelList";
 
 const App = () => {
   return (
     <main>
-      <NextWordPredictor />
+      <ModelList />
     </main>
   );
 };
